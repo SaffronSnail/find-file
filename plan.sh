bef1bc9788c68088da3f8cc0beca39d781aca053
Bryan Ferris <primummoven@gmail.com>
2017-07-25 12:20:15 +0000

    added habitat plan

---
+++ plan.sh
@@ -0,0 +1,32 @@
+pkg_name=find-file
+pkg_origin=saffronsnial
+pkg_versoni=1.0.18
+pkg_license=('unlicense')
+pkg_source=nosuchfile.tar.gz
+pkg_bin_dirs=(bin)
+pkg_build_deps=(core/rust)
+
+do_build() {
+  cargo install find-file --root $pkg_prefix --vers $pkg_version --verbose
+}
+
+do_install() {
+  return 0
+}
+
+do_download() {
+  return 0
+}
+
+do_verify() {
+  return 0
+}
+
+do_unpack() {
+  return 0
+}
+
+do_prepare() {
+  return 0
+}
+
